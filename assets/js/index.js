1ea891fc8789ffac2f78b9e04062c05935f67037
Griga81745 <75415245+Griga81745@users.noreply.github.com>
2022-02-22 14:31:06 +0400

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,5 +0,0 @@
-var activateBurgerMenu = document.getElementById("burger_menu-active")
-activateBurgerMenu.onclick = function () {
-    var burgerMenuActivate = document.getElementById("burgerMenu");
-    burgerMenuActivate.classList.toggle("burgermenu--active");
-}
