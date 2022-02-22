5f66502bfc96813bdc93a6b31a7468534b5b521d
Griga81745 <75415245+Griga81745@users.noreply.github.com>
2022-02-22 14:19:31 +0400

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 var activateBurgerMenu = document.getElementById("burger_menu-active")
 activateBurgerMenu.onclick = function () {
     var burgerMenuActivate = document.getElementById("burgerMenu");
-    burgerMenuActivate.classList.toggle("burgermenu-disable");
+    burgerMenuActivate.classList.toggle("burgermenu--active");
 }
